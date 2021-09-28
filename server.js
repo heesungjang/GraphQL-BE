d2d7ed75b0385ee4fe15b48cc48f54494aa3640e
Heesung jang <heesungj7@gmail.com>
2021-09-29 01:12:22 +0900

    [feat] create Movie by using prisma client

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "prisma/client";
+import { PrismaClient } from ".prisma/client";
 import { ApolloServer, gql } from "apollo-server";
 
 const client = new PrismaClient();
@@ -7,32 +7,43 @@ import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-co
 
 const typeDefs = gql`
     type Movie {
-        id: Int
-        title: String
-        year: Int
+        id: Int!
+        title: String!
+        year: Int!
+        genre: String
+        createdAt: String!
+        updatedAt: String!
     }
     type Query {
         movies: [Movie]
-        movie: Movie
+        movie(id: Int!): Movie
     }
     type Mutation {
-        createMovie(title: String!): Boolean
+        createMovie(title: String!, year: Int!, genre: String): Movie
         deleteMovie(title: String!): Boolean
     }
 `;
 
 const resolvers = {
     Query: {
-        movies: () => [],
-        movie: () => ({ title: "Hello", year: 2021 }),
+        movies: () => client.movie.findMany(),
+        movie: () =>
+            client.movie.findUnique({
+                where: {
+                    id,
+                },
+            }),
     },
     Mutation: {
-        createMovie: (_, { title }) => {
-            console.log(title);
-            return true;
-        },
-        deleteMovie: (_, { title }) => {
-            console.log(title);
+        createMovie: (_, { title, year, genre }) =>
+            client.movie.create({
+                data: {
+                    title,
+                    year,
+                    genre,
+                },
+            }),
+        deleteMovie: (_, { id }) => {
             return true;
         },
     },
