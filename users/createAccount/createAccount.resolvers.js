12fa700d3774be574033702b99e2c04682fae9c2
Heesung jang <heesungj7@gmail.com>
2021-09-30 00:40:32 +0900

    [feat] editProfile typeDef + resolver(mutation)

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,3 +1,6 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
 export default {
     Mutation: {
         createAccount: async (
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,5 +1,36 @@
+import client from "../../client";
+import bcrypt from "bcrypt";
+
 export default {
     Mutation: {
-        editProfile: () => console.log("Hi"),
+        editProfile: async (
+            _,
+            { firstName, lastName, username, email, password: newPassword }
+        ) => {
+            let hashedPassword = null;
+            if (newPassword) {
+                hashedPassword = await bcrypt.hash(newPassword, 10);
+            }
+            const updatedUser = await client.user.update({
+                where: { id: 1 },
+                data: {
+                    firstName,
+                    lastName,
+                    username,
+                    email,
+                    ...(hashedPassword && { password: hashedPassword }),
+                },
+            });
+            if (updatedUser.id) {
+                return {
+                    ok: true,
+                };
+            } else {
+                return {
+                    ok: false,
+                    error: "Could not update user profile",
+                };
+            }
+        },
     },
 };
