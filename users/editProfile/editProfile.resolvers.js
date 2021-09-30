76a5463132a4e85e29a5ce06e57f5bc8900c3331
Heesung jang <heesungj7@gmail.com>
2021-10-01 02:33:26 +0900

    [feat] 이미지 업로드 (read and write stream pipe)

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,3 +1,4 @@
+import { createWriteStream } from "fs";
 import client from "../../client";
 import bcrypt from "bcrypt";
 import { protectedResolver } from "../users.utils";
@@ -20,8 +21,12 @@ export default {
                 { loggedInUser }
             ) => {
                 const { filename, createReadStream } = await avatar;
-                const stream = createReadStream();
-                console.log(stream);
+                const readStream = createReadStream();
+                const writeStream = createWriteStream(
+                    process.cwd() + "/uploads/" + filename
+                );
+
+                readStream.pipe(writeStream);
                 let hashedPassword = null;
                 if (newPassword) {
                     hashedPassword = await bcrypt.hash(newPassword, 10);
