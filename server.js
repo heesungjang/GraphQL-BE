99ef034f3c683195ab319640a3056c3478eaec13
Heesung jang <heesungj7@gmail.com>
2021-10-01 01:12:05 +0900

    [refactor] removed schema

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import schema, { typeDefs, resolvers } from "./schema";
+import { typeDefs, resolvers } from "./schema";
 import { getUser, protectResolver } from "./users/users.utils";
 
 const server = new ApolloServer({
