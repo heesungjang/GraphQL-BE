426c7830fc530efbc65110f932449bca94b4da94
Heesung jang <heesungj7@gmail.com>
2021-10-11 02:04:26 +0900

    apollo express applied + morgan logger

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+import express from "express";
+import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectResolver } from "./users/users.utils";
 
@@ -16,8 +18,10 @@ const server = new ApolloServer({
 
 const PORT = process.env.PORT;
 
-server
-    .listen(PORT)
-    .then(() =>
-        console.log(`🏋️‍♀️ Sever is running on http://localhost:${PORT}/ `)
-    );
+const app = express();
+app.use(logger("tiny"));
+app.use("/static", express.static("uploads"));
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () => {
+    console.log(`🏋️‍♀️ Sever is running on http://localhost:${PORT}/graphql`);
+});
