60173b4643121c46fad03e466ff145ceac9bcbc7
Heesung jang <heesungj7@gmail.com>
2021-10-11 02:05:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser, protectResolver } from "./users/users.utils";
 
-const server = new ApolloServer({
+const apollo = new ApolloServer({
     typeDefs,
     resolvers,
     context: async ({ req }) => {
@@ -21,7 +21,7 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(logger("tiny"));
 app.use("/static", express.static("uploads"));
-server.applyMiddleware({ app });
+apollo.applyMiddleware({ app });
 app.listen({ port: PORT }, () => {
     console.log(`🏋️‍♀️ Sever is running on http://localhost:${PORT}/graphql`);
 });
