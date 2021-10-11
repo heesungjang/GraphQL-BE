80842fdbd5b32b9be01d46d017ef61386ce76b0e
Heesung jang <heesungj7@gmail.com>
2021-10-11 23:42:32 +0900

    message modules

--- messages/seeRoom/seeRoom.resolvers.js
+++ messages/seeRoom/seeRoom.resolvers.js
@@ -3,7 +3,7 @@ import { protectedResolver } from "../../users/users.utils";
 
 export default {
     Query: {
-        seeRooms: protectedResolver(async (_, __, { loggedInUser }) =>
+        seeRoom: protectedResolver(async (_, __, { loggedInUser }) =>
             client.room.findMany({
                 where: {
                     users: {
